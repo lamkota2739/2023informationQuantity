165ec50948f82870a327e419c2427ca1ea1cc7b6
kota2739 <114083014+lamkota2739@users.noreply.github.com>
2023-12-12 16:37:36 +0900

    Test error -1 finded Frequencer.java

--- s4/B233303/Frequencer.java
+++ s4/B233303/Frequencer.java
@@ -47,7 +47,7 @@ public class Frequencer implements FrequencerInterface {
         for(int start = 0; start<spaceLength; start++) { // Is it OK?
             boolean abort = false;
             for(int i = 0; i<targetLength; i++) {
-                if(myTarget[i] != mySpace[start+i]) { abort = true; break; }
+                if(myTarget[i] != mySpace[start+i]) { abort = true; break; } // mySpaceの要素数が[start+i]を超えるとエラー
             }
             if(abort == false) { count++; }
         }
